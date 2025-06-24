a01e2a523da8ac4f1b639ef5da1ebb0139027d78
syedmahmad <devm.ahmad@gmail.com>
2025-06-24 11:39:08 +0500

    updated packages file to remove extras

--- src/components/ClientProviders.tsx
+++ src/components/ClientProviders.tsx
@@ -1,16 +1,14 @@
 'use client';
 
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+// import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from '@/store';
 
 const ClientProviders = ({ children }: { children: React.ReactNode }) => {
     return (
-        <AppRouterCacheProvider>
-            <ReduxProvider store={store}>
-                {children}
-            </ReduxProvider>
-        </AppRouterCacheProvider>
+        <ReduxProvider store={store}>
+            {children}
+        </ReduxProvider>
     );
 };
 
