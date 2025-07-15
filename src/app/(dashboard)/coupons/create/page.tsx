6140217536b3ec2a901ed344f9800f9047bef105
deepakRampwin <deepak.mahato@rampwin.com>
2025-07-15 12:43:05 +0530

    disabled isAlreadySelected coupon

--- src/app/(dashboard)/coupons/create/page.tsx
+++ src/app/(dashboard)/coupons/create/page.tsx
@@ -618,14 +618,23 @@ const CreateCouponForm = ({
                             }
                           >
                             {couponTypes.map(
-                              (option: { id: number; coupon_type: string }) => (
-                                <MenuItem
-                                  key={option.coupon_type}
-                                  value={option.id}
-                                >
-                                  {option.coupon_type}
-                                </MenuItem>
-                              )
+                              (option: { id: number; coupon_type: string }) => {
+                                const isAlreadySelected =
+                                  dynamicCouponTypesRows.some(
+                                    (row, index) =>
+                                      Number(row.coupon_type) === option.id &&
+                                      index !== couponTypesRowIndex
+                                  );
+                                return (
+                                  <MenuItem
+                                    key={option.coupon_type}
+                                    value={option.id}
+                                    disabled={isAlreadySelected}
+                                  >
+                                    {option.coupon_type}
+                                  </MenuItem>
+                                );
+                              }
                             )}
                           </TextField>
                         </Grid>
